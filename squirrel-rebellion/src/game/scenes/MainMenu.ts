22c74f29e483d6d5cc36839f5d35da16aeac6bf2
Tim Schaefer <ts+code@rcmd.org>
2026-02-21 17:44:31 +0100

    NEW: add main menu graphics

--- squirrel-rebellion/src/game/scenes/MainMenu.ts
+++ squirrel-rebellion/src/game/scenes/MainMenu.ts
@@ -15,18 +15,22 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.background = this.add.image(512, 384, 'background');
+        this.cameras.main.setBackgroundColor(0x111111); // around the image
+        this.background = this.add.image(512, 450, 'background');
 
-        this.logo = this.add.image(512, 250, 'logo');
 
-        this.title = this.add.text(512, 380, 'Main Menu', {
+        //this.logo = this.add.image(512, 400, 'logo');
+        const menuStartY = 580;
+        const menuItemsYDistance = 60;
+
+        this.title = this.add.text(512, menuStartY, 'Main Menu', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
 
         // Start Game button
-        this.startButton = this.add.text(512, 460, 'Start Game', {
+        this.startButton = this.add.text(512, menuStartY + menuItemsYDistance, 'Start Game', {
             fontFamily: 'Arial Black', fontSize: 32, color: '#ffffff',
             stroke: '#000000', strokeThickness: 6,
             align: 'center'
@@ -38,7 +42,7 @@ export class MainMenu extends Scene
         .on('pointerdown', () => this.scene.start('Game'));
 
         // Credits button
-        this.creditsButton = this.add.text(512, 520, 'Credits', {
+        this.creditsButton = this.add.text(512, menuStartY + 2 * menuItemsYDistance, 'Credits', {
             fontFamily: 'Arial Black', fontSize: 32, color: '#ffffff',
             stroke: '#000000', strokeThickness: 6,
             align: 'center'
